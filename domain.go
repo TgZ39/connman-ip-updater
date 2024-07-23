b61790d02adc70c695264ca5d85386072b04ad76
TgZ39 <badbaboimbus+github@gmail.com>
2024-07-23 18:48:44 +0200

    add retry logic to domain lookup

--- domain.go
+++ domain.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"log"
 	"net"
+	"time"
 )
 
 func GetIpFromDomain(domain string) (net.IP, error) {
-	ips, err := net.LookupIP(domain)
-	if err != nil {
-		return nil, err
+	var ips []net.IP
+	var err error
+
+	for i := 0; i < 5; i++ {
+		ips, err = net.LookupIP(domain)
+		if err != nil {
+			log.Println("error looking up IP: ", err)
+			log.Println("trying again in 5 seconds...")
+			time.Sleep(5 * time.Second)
+		} else {
+			return ips[0], err
+		}
 	}
 
-	return ips[0], nil
+	return nil, err
 }
